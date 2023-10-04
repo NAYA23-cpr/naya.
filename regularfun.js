8546cdd3922c5bc629269cc1bf98f453c4fd5bf6
NAYA23-cpr <141000562+NAYA23-cpr@users.noreply.github.com>
2023-10-04 14:59:53 +0700

    Add files via upload

---
+++ regularfun.js
@@ -0,0 +1,8 @@
+function login(user, pass){
+  console.log("--------------------------------")
+  console.log("           login")
+  console.log("username" + user)
+  console.log("pass" + pass)
+  console.log("--------------------------------")
+}
+login("naya hinayatulloh" , 123456)
